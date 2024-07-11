bfab72eb22d452aee059748e99cb738b8339cda4
PedroCr13 <lopes.pc.2019@gmail.com>
2024-07-11 07:08:43 -0300

    Generic lista

---
+++ MyGenericApp.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyGenericAPP {
+	public static void main(String args[]){
+		
+		List<Integer> lista = new ArrayList<Integer>();
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		
+		int total = 0;
+		for(Integer i : lista){
+			total += i;
+		}
+		
+		System.out.println(total);
+	}
+}
